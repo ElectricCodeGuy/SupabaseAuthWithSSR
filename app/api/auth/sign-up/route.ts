647b9ede7a0f90769ca46cb01653d6e561c76634
Oscar Lauge Hoffmann <137044109+ElectricCodeGuy@users.noreply.github.com>
2023-10-31 18:44:17 +0100

    Update route.ts

--- app/api/auth/sign-up/route.ts
+++ app/api/auth/sign-up/route.ts
@@ -23,7 +23,7 @@ export async function POST(request: Request) {
 
   if (error) {
     return NextResponse.redirect(
-      `${requestUrl.origin}/login?error=Could not authenticate user`,
+      `${requestUrl.origin}/auth/signin?error=Could not authenticate user`,
       {
         // a 301 status is required to redirect from a POST to a GET route
         status: 301
