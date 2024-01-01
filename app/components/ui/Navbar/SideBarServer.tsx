bcbafd80779bc35f06b63ade3607e785b4c51fe2
ElectricCodeGuy <s170559@student.dtu.dk>
2024-01-01 15:15:44 +0100

    Bump To Newest Version and Updated to use new Mui AppRouterCacheWrapper

--- app/components/ui/Navbar/SideBarServer.tsx
+++ app/components/ui/Navbar/SideBarServer.tsx
@@ -1,14 +1,8 @@
-import { createClient } from '@/lib/server/server';
 import Sidebar from './SideBar';
-import { cookies } from 'next/headers'; // Import the cookies function from next/headers
+import { getSession } from '@/lib/client/supabase'; // Import getSession
 
 export default async function NavBar() {
-  const cookieStore = cookies(); // Initialize cookieStore using cookies function
-  const supabase = createClient(cookieStore); // Pass cookieStore as an argument
-
-  // Extract the session data from the data field of the returned object
-  const { data } = await supabase.auth.getSession();
-  const session = data?.session || null;
+  const session = await getSession(); // Get session
 
   // Console logging the session data
   console.log('Session data:', session);
