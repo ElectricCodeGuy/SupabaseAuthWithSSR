c0f8fe27f1a1330abadd9ef6657e97fdacad80fb
ElectricCodeGuy <hoffmann.oscar123@hotmail.com>
2025-04-12 02:50:13 +0200

    clean up some code and fix some bugs and bump devs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
-import React, { type ReactNode, Suspense } from 'react';
+import React, { type ReactNode } from 'react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Footer from '@/app/components/ui/Footer/Footer';
 import RootErrorBoundary from '@/app/components/errorBoundary/ErrorBoundaryPage';
 import { getSession } from '@/lib/server/supabase';
 import NavBar from '@/app/components/ui/Navbar/TopBar';
-import SnackbarMessages from './components/ui/SnackbarMessage';
 import { ThemeProvider } from '@/components/ui/theme-provider';
+import { Toaster } from '@/components/ui/sonner';
 
 import './global.css';
 
@@ -50,11 +50,9 @@ export default function RootLayout({
             {/* We pass the promise here and resolve it with react.use in the child to prevent the async request from blocking the UI */}
             <NavBar session={getSession()} />
             <main>{children}</main>
+            <Toaster />
             {modal}
             <Footer />
-            <Suspense fallback={null}>
-              <SnackbarMessages />
-            </Suspense>
           </ThemeProvider>
         </body>
       </RootErrorBoundary>
